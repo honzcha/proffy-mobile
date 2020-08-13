38b4659123e8a70843432a798800cda984e0ffd3
Lucas Garcia <honzcha@outlook.com>
2020-08-13 13:35:13 +0800

    Give Classes page

---
+++ src/pages/GiveClasses/index.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import { View } from "react-native";
+
+import styles from "./styles";
+
+function GiveClasses() {
+  return(
+
+  <View style={styles.container}>
+    
+    
+  </View>)
+}
+
+export default GiveClasses;
--- src/pages/Landing/index.tsx
+++ src/pages/Landing/index.tsx
@@ -1,22 +1,52 @@
 import React from "react";
-import { View, Image, Text } from "react-native";
+import { View, Image, Text, TouchableOpacity } from "react-native";
+import { useNavigation } from "@react-navigation/native";
+import { RectButton } from "react-native-gesture-handler";
 
-import styles from "./styles.ts";
+RectButton;
 
-import landingImg from "../../images/landing.png";
+import styles from "./styles";
+
+import landingImg from "../../assets/images/landing.png";
+import studyIcon from "../../assets/images/icons/study.png";
+import giveClassesIcon from "../../assets/images/icons/give-classes.png";
+import heartIcon from "../../assets/images/icons/heart.png";
 
 function Landing() {
+  const { navigate } = useNavigation();
+
+  function handleNavigateToGiveClassesPage() {
+    navigate("GiveClasses");
+  }
+
   return (
     <View style={styles.container}>
       <Image source={landingImg} style={styles.banner} />
 
       <Text style={styles.title}>
-        Seja bem-vindo, {"\n"}
-        <Text style={styles.titleBold}>O que deseja fazer?</Text>
+        Welcome, {"\n"}
+        <Text style={styles.titleBold}>What do you want to do?</Text>
+      </Text>
+      <View style={styles.buttonsContainer}>
+        <RectButton style={[styles.button, styles.buttonPrimary]}>
+          <Image source={studyIcon} />
+          <Text style={styles.buttonText}>Study</Text>
+        </RectButton>
+
+        <RectButton
+          onPress={handleNavigateToGiveClassesPage}
+          style={[styles.button, styles.buttonSecondary]}
+        >
+          <Image source={giveClassesIcon} />
+          <Text style={styles.buttonText}>Give Classes</Text>
+        </RectButton>
+      </View>
+
+      <Text style={styles.totalConnections}>
+        Total of XXX connections already made! <Image source={heartIcon} />
       </Text>
     </View>
   );
 }
 
 export default Landing;
-//test
